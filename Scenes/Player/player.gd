0ce5891e185f80d37a6235c00ed02be504777d16
MargueriteLaVache <mayeul.hospital@free.fr>
2023-09-11 15:15:39 +0200

    Auto-jump fixed more or less
    The character now auto "jumps" upon contact with a platform (or anything really). This is halted at the beginning, starting only when the player hits the space key. One issue is that there is a limited number of jumps possible. Plus the jumping isn't very elegant.

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -6,7 +6,6 @@ extends CharacterBody2D
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- Scenes/Player/playerbis.gd
+++ Scenes/Player/playerbis.gd
@@ -1,26 +1,46 @@
 extends RigidBody2D
 
-var thrust = Vector2(0, -1000)
+var thrust = Vector2(0, -2000)
+var lateral_thrust = Vector2(0, -400)
 var torque = 20000
+var jump1 = false
+var jump2 = false
 
 func _integrate_forces(state):
-	if Input.is_action_pressed("ui_up"):
+	# if Input.is_action_pressed("ui_up"):
+	if jump1 and jump2:
 		state.apply_central_force(thrust)
 	else:
 		state.apply_force(Vector2())
 	var rotation_direction = 0
 	if Input.is_action_pressed("ui_right"):
 		rotation_direction += 1
-		state.apply_central_force(thrust.rotated(rotation_direction * PI / 2))  # This is done here to prevent it from being always active.
+		state.apply_central_force(lateral_thrust.rotated(rotation_direction * PI / 2))  # This is done here to prevent it from being always active.
 	if Input.is_action_pressed("ui_left"):
 		rotation_direction -= 1
-		state.apply_central_force(thrust.rotated(rotation_direction * PI / 2))
+		state.apply_central_force(lateral_thrust.rotated(rotation_direction * PI / 2))
 	# state.apply_torque(rotation_direction * torque)
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_contact_monitor(true)
+	set_max_contacts_reported(1000)  # Au pif value, to be changed
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("start"):
+		jump2 = true
+
+
+func _on_body_entered(body):
+	print("hello")
+	jump1 = true
+	
+
+
+func _on_body_exited(body):
+	print("goodbye")
+	await get_tree().create_timer(0.3).timeout
+	jump1 = false
